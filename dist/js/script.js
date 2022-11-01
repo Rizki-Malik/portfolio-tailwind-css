c0cde32139f271f56a3a618ef11e68985fefd993
DariusTorto <rizkimalik221001@gmail.com>
2022-11-02 06:19:40 +0700

    menghilangkan blob mode handphone

---
+++ dist/js/script.js
@@ -0,0 +1,60 @@
+//animasi hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+const toTop = document.querySelector('#to-top');
+
+
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+//navbar fixed
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+// klik diluar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// pindahkan modetoggle dark
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+  }
